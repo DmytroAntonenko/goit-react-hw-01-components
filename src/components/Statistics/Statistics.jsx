0391d7cac404753df20b25b0d2a16a68e5af5b80
DmytroAntonenko <d.antonenko81@gmail.com>
2022-12-01 17:13:05 +0200

    1.6

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,29 +1,29 @@
 import css from "../Statistics/Statistics.module.css";
+import PropTypes from 'prop-types';
 export const Statistics = ( {title, stats} ) => {
     // console.log(username, tag, location, avatar, followers, views, likes)
-      return <section className={css.form__wrap}>
-        <h2 class="title">{title}</h2>
+      return <section className={css.section}>
+        {title && <h2 className={css.title}>{title}</h2>}
+        {/* <h2 class="title">{title}</h2> */}
 
-        <ul class="stat-list">
+        <ul className={css.statList}>
             {stats.map(item => (
-                <li key={item.id} class="item">
-                <span class="label">{item.label}</span>
-                <span class="percentage">{item.percentage}</span>
+                <li key={item.id} className={css.item}>
+                    <span className={css.label}>{item.label}</span>
+                    <span className={css.percentage}>{item.percentage}%</span>
             </li>))
             }
-            
-            {/* <li class="item">
-                <span class="label">.mp3</span>
-                <span class="percentage">14%</span>
-            </li>
-            <li class="item">
-                <span class="label">.pdf</span>
-                <span class="percentage">41%</span>
-            </li>
-            <li class="item">
-                <span class="label">.mp4</span>
-                <span class="percentage">12%</span>
-            </li> */}
         </ul>
     </section>
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+        PropTypes.exact({
+            id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired, 
+            percentage: PropTypes.number.isRequired,
+        })
+    )
 }
